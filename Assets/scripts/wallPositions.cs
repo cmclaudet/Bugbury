c7f16aa646f99be30e3627d2805186e50586157a
Catherine Claudet <cmclaudet@gmail.com>
2017-01-28 12:59:14 -0500

    Updated colors
    - Fixed button pressed shade
    - Move walls to actual screen edge

---
+++ Assets/scripts/wallPositions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wallPositions : MonoBehaviour {
+    //sets wall size and position based on screen size
+    private EdgeCollider2D[] walls;
+    private Vector3 worldScreenDim;
+
+	// Use this for initialization
+	void Start () {
+        walls = GetComponents<EdgeCollider2D>();
+        Vector3 pixelScreenDim = new Vector3(Screen.width, Screen.height, 0.0f);
+        worldScreenDim = Camera.main.ScreenToWorldPoint(pixelScreenDim);
+
+		walls[0].offset = new Vector2(2*worldScreenDim.x, 0);
+		walls[1].offset = new Vector2(-2*worldScreenDim.x, 0);
+     }
+	
+}
