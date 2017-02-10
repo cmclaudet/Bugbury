c7b1530fb4a5bbe3b0be88dfd28a041bad49a2da
Catherine Claudet <cmclaudet@gmail.com>
2017-02-10 14:48:52 +0100

    Added 3rd level to level select menu
    -  Changed level select so that 3 levels can be accessed from level
    select screen

--- Assets/scripts/panMenu1.cs
+++ Assets/scripts/panMenu1.cs
@@ -2,31 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 //triggers panning animation when player presses forward button to focus on next level
 public class panMenu1 : MonoBehaviour {
 	public Button backButton;
 	public Animator lvl1;
 	public Animator lvl2;
+	public Animator lvl3;
 	public AudioSource woosh;
+
+	public bool onLevel2{ get; set; }
 
 	// Use this for initialization
 	void Start () {
-		//set to false to ensure default animation is only played when player presses the button, not on scene load
+		backButton.interactable = false;
 		lvl1.speed = 2.0f;
 		lvl2.speed = 2.0f;
+		lvl3.speed = 2.0f;
+
+		//set to false to ensure default animation is only played when player presses the button, not on scene load
 		lvl1.enabled = false;
 		lvl2.enabled = false;
+
+		onLevel2 = false;
 	}
 
 	public void Press() {
+		if (onLevel2) {
+			From2To3 ();
+		} else {
+			From1To2 ();
+		}
+	}
+
+	void From1To2() {
 		woosh.Play ();
 		lvl1.enabled = true;
 		lvl2.enabled = true;
-		lvl1.Play ("pan");
-		lvl2.Play ("pan 1");
-		GetComponent<Button> ().interactable = false;
+		lvl1.Play ("panCenterLeft");
+		lvl2.Play ("panRightCenter");
+
 		backButton.interactable = true;
+		onLevel2 = true;
 	}
 
+	void From2To3() {
+		lvl3.gameObject.SetActive (true);
+		lvl1.gameObject.SetActive (false);
+
+		woosh.Play ();
+		lvl2.Play ("panCenterLeft");
+		lvl3.Play ("panRightCenter");
+
+		GetComponent<Button> ().interactable = false;
+		onLevel2 = false;
+	}
 }
--- Assets/scripts/panMenu2.cs
+++ Assets/scripts/panMenu2.cs
@@ -8,20 +8,48 @@ public class panMenu2 : MonoBehaviour {
 	public Button forwardButton;
 	public Animator lvl1;
 	public Animator lvl2;
+	public Animator lvl3;
 	public AudioSource woosh;
+
+	private bool onLevel2;
 
 	// Use this for initialization
 	void Start () {
 		lvl1.speed = 2.0f;
 		lvl2.speed = 2.0f;
+		lvl3.speed = 2.0f;
 		GetComponent<Button> ().interactable = false;	//set to false as player can only go back once they have gone forward
+
+		onLevel2 = false;
 	}
 
 	public void PressBack() {
+		onLevel2 = forwardButton.GetComponent<panMenu1> ().onLevel2;
+
+		if (onLevel2) {
+			From2To1 ();
+		} else {
+			From3To2 ();
+		}
+	}
+
+	void From2To1() {
+		lvl3.gameObject.SetActive (false);
+		lvl1.gameObject.SetActive (true);
 		woosh.Play ();
-		lvl1.Play ("panb");		//back panning for level 1 select menu
-		lvl2.Play ("pan 1b");	//back panning for level 2 select menu
+		lvl1.Play ("panLeftCenter");		//back panning for level 1 select menu
+		lvl2.Play ("panCenterRight");	//back panning for level 2 select menu
+
 		GetComponent<Button> ().interactable = false;
-		forwardButton.interactable = true;	//player can only go forward once they have gone back
+		forwardButton.GetComponent<panMenu1> ().onLevel2 = false;
+	}
+
+	void From3To2() {
+		woosh.Play ();
+		lvl2.Play ("panLeftCenter");
+		lvl3.Play ("panCenterRight");
+
+		forwardButton.interactable = true;
+		forwardButton.GetComponent<panMenu1> ().onLevel2 = true;
 	}
 }
