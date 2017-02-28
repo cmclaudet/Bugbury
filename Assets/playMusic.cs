a671577137088971489b0b89f299741aef82038e
Catherine Claudet <cmclaudet@gmail.com>
2017-02-28 14:35:17 +0100

    Music continues from title
    - Music starts in title and continues in choose level and credits scene
    and continues on going back to title

---
+++ Assets/playMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playMusic : MonoBehaviour {
+	public AudioSource music;
+	// Use this for initialization
+	void Start () {
+		if (!musicManager.Instance.isPlaying) {
+			AudioSource newMusic = Instantiate (music);
+			newMusic.Play ();
+			musicManager.Instance.isPlaying = true;
+			musicManager.Instance.music = newMusic.gameObject;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/musicManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicManager : MonoBehaviour {
+
+	private static musicManager _instance;
+
+	public static musicManager Instance {
+		get {
+			if (_instance == null) {
+				GameObject go = new GameObject ("musicManager");
+				go.AddComponent<musicManager> ();
+			}
+			return _instance;
+		}
+	}
+
+	public bool isPlaying;
+	public GameObject music;
+
+	void Awake() {
+		_instance = this;
+		isPlaying = false;
+		DontDestroyOnLoad (this.gameObject);
+	}
+
+}
