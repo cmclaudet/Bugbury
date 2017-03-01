56907706444f1ae841ce0319ed0c6493f57e8f05
Catherine Claudet <cmclaudet@gmail.com>
2017-03-01 13:02:02 +0100

    Created prefab menu for rating app
    - Adjusted scale setup script to allow object to scale down from a
    public function
    - Attached scale down function to “No thanks” button for rating the app
    - Attached opening product URL on play store to “Sure!” Button

--- Assets/scripts/scaling/scaleSetup.cs
+++ Assets/scripts/scaling/scaleSetup.cs
@@ -17,18 +17,22 @@ public class scaleSetup : MonoBehaviour {
 	public float startScale;		//start scale, ie value which object starts at
 	public bool lowTimeStep;		//set to true if scaling needs many checks per second to be precise
 	public string desiredSoundEffect;	//input name of desired sound effect in hierarchy. If no name is placed there will be no sound effect
+	public string downscaleSound;
 
 	private AudioSource soundEffect;
-	private float finalScale;	//final value object scales to
-	private bool scalingUp;		//is true is object is growing from small to big. false otherwise.
+	private AudioSource soundEffectDownscale;
+	public float finalScale{ get; set; }	//final value object scales to
+	private bool scalingUp;		//is true if object is growing from small to big. false otherwise.
 	private float timePassed;
 	private bool countTime;		//checks when timeDelay has passsed
 	public bool needScaling{ get; set; }	//ensures object is only scaled when it needs to be
 	public bool doneScaling{ get; set; }	//ensures fixed delta time is reverted to its original number only once (right after scaling is done), not repeatedly
+	private bool scalingDown;				//set to true when scaling down. allows gameobject to inactivate once scaling down is done
 
 	void Awake() {
 		needScaling = false;	//set to false so that object only scales after time delay
 		doneScaling = false;
+		scalingDown = false;
 	}
 
 	// Use this for initialization
@@ -63,12 +67,20 @@ public class scaleSetup : MonoBehaviour {
 		if (!doneScaling) {
 			checkScalingDone ();
 		}
+
+		//if object scales down afterwards must be set to inactive once downscaling is complete
+		if (doneScaling && scalingDown) {
+			gameObject.SetActive (false);
+		}
 	}
 
 	void setSoundEffect() {
 		//if no name placed no sound effect is set
 		if (desiredSoundEffect != "") {
 			soundEffect = GameObject.Find (desiredSoundEffect).GetComponent<AudioSource> ();
+		}
+		if (downscaleSound != "") {
+			soundEffectDownscale = GameObject.Find (downscaleSound).GetComponent<AudioSource> ();
 		}
 	}
 
@@ -126,5 +138,18 @@ public class scaleSetup : MonoBehaviour {
 		needScaling = false;
 		Time.fixedDeltaTime = 0.02f;
 		doneScaling = true;
+	}
+
+	public void startScalingDown() {
+		countTime = true;
+		timePassed = 0;
+		timeDelay = 0;
+		soundEffect = soundEffectDownscale;
+		scalingObj.velocity = -vel;
+		scalingObj.acceleration = acc;
+		finalScale = 0;
+		scalingUp = checkScalingDir ();
+		doneScaling = false;
+		scalingDown = true;
 	}
 }
