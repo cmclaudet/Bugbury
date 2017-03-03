7a627c8fedc14b8475e77b1b86b57e3af69ad476
Catherine Claudet <cmclaudet@gmail.com>
2017-03-03 16:24:58 +0100

    Add mute to level 1
    - Mute button in top right to toggle sound on and off

---
+++ Assets/mute.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mute : MonoBehaviour {
+	public bool isMuted{get;set;}
+
+	void Start() {
+		isMuted = false;
+	}
+
+	public void toggleMute() {
+		if (isMuted) {
+			AudioListener.volume = 1.0f;
+			isMuted = false;
+		} else {
+			AudioListener.volume = 0;
+			isMuted = true;
+		}
+	}
+}
