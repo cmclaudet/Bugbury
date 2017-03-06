3003e1bbd7bb706645e64e2a662982739f776e38
Catherine Claudet <cmclaudet@gmail.com>
2017-03-06 10:07:05 +0100

    Changed mute button sprite
    - Mute button sprite switches between megaphone and megaphone crossed
    out when mute is turned off and on.
    - Changed life images to hearts

--- Assets/mute.cs
+++ Assets/mute.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class mute : MonoBehaviour {
 	public bool isMuted{get;set;}
+	public Sprite muteOn;
+	public Sprite muteOff;
 
 	void Start() {
 		isMuted = false;
@@ -11,9 +14,11 @@ public class mute : MonoBehaviour {
 
 	public void toggleMute() {
 		if (isMuted) {
+			GetComponent<Image> ().sprite = muteOn;
 			AudioListener.volume = 1.0f;
 			isMuted = false;
 		} else {
+			GetComponent<Image> ().sprite = muteOff;
 			AudioListener.volume = 0;
 			isMuted = true;
 		}
