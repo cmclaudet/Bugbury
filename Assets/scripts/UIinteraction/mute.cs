53000c0afac1116e0c522e78fcdea8b7386aa180
Catherine Claudet <cmclaudet@gmail.com>
2017-03-07 14:04:45 +0100

    Included mute button on title screen
    - Added mute button to title screen, credits and level select
    - Added isMuted bool to music manager so that muting sound will not be
    reset on loading different scenes

--- Assets/scripts/UIinteraction/mute.cs
+++ Assets/scripts/UIinteraction/mute.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class mute : MonoBehaviour {
-	public bool isMuted{get;set;}
 	public Sprite muteOn;
 	public Sprite muteOff;
+	private bool isMuted;
 
 	void Start() {
-		isMuted = false;
+		isMuted = musicManager.Instance.isMuted;
+		updateImageAndSound ();
 	}
 
 	public void toggleMute() {
 		if (isMuted) {
+			musicManager.Instance.isMuted = false;
+		} else {
+			musicManager.Instance.isMuted = true;
+		}
+		isMuted = musicManager.Instance.isMuted;
+
+		updateImageAndSound ();
+	}
+
+	void updateImageAndSound() {
+		if (isMuted) {
 			GetComponent<Image> ().sprite = muteOn;
-			AudioListener.volume = 1.0f;
-			isMuted = false;
+			AudioListener.volume = 0;
 		} else {
 			GetComponent<Image> ().sprite = muteOff;
-			AudioListener.volume = 0;
-			isMuted = true;
+			AudioListener.volume = 1.0f;
 		}
 	}
 }
