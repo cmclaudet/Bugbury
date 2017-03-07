7d044e4089c3d3ad9cf5ac0d94e168301bcdc43e
Catherine Claudet <cmclaudet@gmail.com>
2017-03-07 14:45:32 +0100

    Android back button takes players back
    - On level select and credits, pressing the back button on an android
    device will take them back to the title scene
    - On title scene, pressing back will quit the application

---
+++ Assets/detectAndroidBackButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class detectAndroidBackButton : MonoBehaviour {
+	private string thisScene;
+
+	void Start() {
+		thisScene = SceneManager.GetActiveScene ().name;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Application.platform == RuntimePlatform.Android) {
+			if (Input.GetKey (KeyCode.Escape)) {
+				switch (thisScene) {
+				case "title":
+					Application.Quit ();
+					break;
+				case "levelSelect":
+					SceneManager.LoadScene ("title");
+					break;
+				case "credits":
+					SceneManager.LoadScene ("title");
+					break;
+				}
+
+				return;
+			}
+		}
+	}
+}
