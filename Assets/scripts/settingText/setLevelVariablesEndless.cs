3b3cccc34d88f91c595c70e69c6e863204bbd466
Catherine Claudet <cmclaudet@gmail.com>
2017-03-16 16:24:19 +0100

    Added scripts for endless mode
    - Added endless mode scripts for caterpillar manager, move and set
    scene variables

---
+++ Assets/scripts/settingText/setLevelVariablesEndless.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//place on object in hierarchy so that public values can be set according to scene, ie level.
+//values are set in singleton manager classes so they can be easily referred to in other scripts
+public class setLevelVariablesEndless : MonoBehaviour {
+	public int farBonus;
+
+	// Use this for initialization
+	void Awake () {
+		scoreCount.Instance.farShotBonus = farBonus;
+	}
+
+}
---
+++ Assets/scripts/singletons/caterpillarManagerEndless.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//script manages caterpillar spawn frequency changing over time, keeps count of caterpillar deaths and triggers end level message when all caterpillars are inactivated
+//takes minimum and maximum caterpillar speeds as arguments. caterpillar speed increases linearly over spawn number up to total caterpillar number
+//removed life from player if player misses caterpillar
+public class caterpillarManagerEndless : MonoBehaviour {
+
+	private static caterpillarManagerEndless _instance;
+
+	public static caterpillarManagerEndless Instance {
+		get {
+			if (_instance == null) {
+				GameObject go = new GameObject ("caterpillarManagerEndless");
+				go.AddComponent<caterpillarManagerEndless> ();
+			}
+			return _instance;
+		}
+	}
+
+	public float interbugDistance = 3;	//desired distance between subsequently spawning bugs
+	public int lanes = 6;			//number of lanes caterpillars spawn on
+	public float finishLine = -3.0f;	//finish line y co-ordinated in world space
+	public float cameraScoreNumShakeDuration = 0.5f;	//camera shake duration for landing of score number
+	public bool levelOngoing{get;set;}
+
+	//values are set in the inspector and set to this class as they are scene specific
+	public float minVel = 0.5f;		//minimum possible velocity for caterpillar
+	public float velocityIncrement = 0.01f;
+	public Rigidbody2D caterpillars{ get; set; }	//caterpillar prefab
+
+	public int currentSpawn{ get; set; }	//number of the most recent caterpillar
+	public bool levelStart{get;set;}
+	public int caterpillarsInactivated{ get; set; }		//equals caterpillars killed + caterpillars passed over finish line
+	public int caterpillarsKilled{ get; set; }			//total caterpillars player successfully killed
+	private float minimunY;			//minimum possible y value for caterpillars before going inactive
+
+
+	private float spawnFrequency;	//changes depending on speed of caterpillar to keep interbugDistance constant
+	private float timeSinceSpawn;
+	private GameObject[] allCaterpillars;
+
+	void Awake() {
+		_instance = this;
+	}
+
+	void Start() {
+		currentSpawn = 0;
+		levelStart = false;
+		caterpillarsInactivated = 0;
+		caterpillarsKilled = 0;
+		findAllBugs ();		//finds all caterpillars currently in heirarchy
+		minimunY = findMinY();
+
+		timeSinceSpawn = spawnFrequency;	//set so that caterpillar spawns right away
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (levelStart) {
+			timeSinceSpawn += Time.deltaTime;
+		}
+
+		//instantiate caterpillar when time has elapsed over spawn frequency
+		if (timeSinceSpawn > spawnFrequency && levelOngoing) {
+			Instantiate (caterpillars);
+			timeSinceSpawn = 0;
+			currentSpawn += 1;
+			calculateSpawnTime ();	//find spawn time for next caterpillar
+		}
+
+
+		for (int i = 0; i < allCaterpillars.Length; i++) {
+			//inactivates caterpillars if they have surpassed finish line
+			if (allCaterpillars [i].transform.position.y < minimunY) {
+				allCaterpillars [i].gameObject.SetActive (false);
+				caterpillarsInactivated += 1;
+				lifeManager.Instance.lifeLost = true;	//triggers removal of one of player's lives
+
+				//resets player combo and checks if max streak has been overtaken
+				resetMaxStreak ();
+				scoreCount.Instance.playerCombo = 0;
+				findAllBugs ();		//recount caterpillars after inactivation
+
+			}
+		}
+	}
+
+	void calculateSpawnTime() {
+		findAllBugs ();
+
+		//get velocity of most recently spawned caterpillar
+		float currVel = -allCaterpillars [allCaterpillars.Length - 1].GetComponent<Rigidbody2D> ().velocity.y;
+		//time=distance/speed
+		spawnFrequency = interbugDistance / currVel;
+	}
+
+	void findAllBugs() {
+		allCaterpillars = GameObject.FindGameObjectsWithTag ("caterpillar");
+	}
+
+	float findMinY() {
+		float yMin = finishLine - 0.5f;
+		return yMin;
+	}
+
+	public void resetMaxStreak() {
+		if (scoreCount.Instance.playerCombo > scoreCount.Instance.maxPlayerStreak) {
+			scoreCount.Instance.maxPlayerStreak = scoreCount.Instance.playerCombo;
+		}
+	}
+
+
+}
