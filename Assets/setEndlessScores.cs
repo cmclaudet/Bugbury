1c2694791eb2cf1e9a1c05e62b95ca24b454efb0
Catherine Claudet <cmclaudet@gmail.com>
2017-03-17 15:34:13 +0100

    Created level select menu for endless level
    - Created separate scene for level select on endless
    - Created level select menu for endless levels
    - Created script to set endless high scores to level select menu

---
+++ Assets/setEndlessScores.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class setEndlessScores : MonoBehaviour {
+	public Transform level1;
+	public Transform level2;
+	public Transform level3;
+	public Transform level4;
+	public Transform level5;
+
+	// Use this for initialization
+	void Start () {
+		highScore ();
+	}
+
+	//updates visible highscore
+	void highScore() {
+		level1.GetComponentsInChildren<Text>()[1].text = "High Score: " + highScoreManager.Instance.One.HSEndless;
+		level2.GetComponentsInChildren<Text>()[1].text = "High Score: " + highScoreManager.Instance.Two.HSEndless;
+		level3.GetComponentsInChildren<Text>()[1].text = "High Score: " + highScoreManager.Instance.Three.HSEndless;
+		level4.GetComponentsInChildren<Text>()[1].text = "High Score: " + highScoreManager.Instance.Four.HSEndless;
+		level5.GetComponentsInChildren<Text>()[1].text = "High Score: " + highScoreManager.Instance.Five.HSEndless;
+	}
+
+
+}
