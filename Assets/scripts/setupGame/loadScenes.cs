fd7e26886830e5c819171f4fcbcd1e3083e6c026
Catherine Claudet <cmclaudet@gmail.com>
2017-03-17 15:46:30 +0100

    Added endless level select menus for all levels
    - Modified load next level function so that it loads on correct mode
    (endless if scene is already endless)
    - Added load endless levels function to load scenes script

--- Assets/scripts/setupGame/loadScenes.cs
+++ Assets/scripts/setupGame/loadScenes.cs
@@ -10,10 +10,24 @@ public class loadScenes : MonoBehaviour {
 	private AudioSource click;
 
 	private string[] allLevels;
+	private string[] allLevelsEndless;
+
+	private bool endless;
 
 	void Awake() {
 		click = GameObject.Find ("click").GetComponent<AudioSource>();
 		allLevels = new string[] { "level 1", "level 2", "level 3", "level 4", "level 5" };
+		allLevelsEndless = new string[] {
+			"level 1 endless",
+			"level 2 endless",
+			"level 3 endless",
+			"level 4 endless",
+			"level 5 endless"
+		};
+	}
+
+	void Start() {
+		endless = caterpillarManager.Instance.endlessLevel;
 	}
 
 	//load from level complete
@@ -25,9 +39,17 @@ public class loadScenes : MonoBehaviour {
 
 	//load from level complete
 	public void nextLevel() {
-		for (int i = 0; i < allLevels.Length; i++ ) {
-			if (SceneManager.GetActiveScene ().name == allLevels [i]) {
-				SceneManager.LoadScene (allLevels [i + 1]);
+		if (endless) {
+			for (int i = 0; i < allLevels.Length; i++) {
+				if (SceneManager.GetActiveScene ().name == allLevels [i]) {
+					SceneManager.LoadScene (allLevels [i + 1]);
+				}
+			}
+		} else {
+			for (int i = 0; i < allLevelsEndless.Length; i++) {
+				if (SceneManager.GetActiveScene ().name == allLevelsEndless [i]) {
+					SceneManager.LoadScene (allLevelsEndless [i + 1]);
+				}
 			}
 		}
 		click.Play ();
@@ -84,6 +106,23 @@ public class loadScenes : MonoBehaviour {
 		Time.timeScale = 1.0f;
 		SceneManager.LoadScene ("levelSelect");
 		click.Play ();
+	}
+
+	public void loadChooseEndlessLevel() {
+		if (musicManager.Instance.isPlaying) {
+			DontDestroyOnLoad (musicManager.Instance.music);
+		} else {
+			AudioSource newMusic = Instantiate (music);
+			newMusic.Play ();
+			musicManager.Instance.isPlaying = true;
+			musicManager.Instance.music = newMusic.gameObject;
+			DontDestroyOnLoad (musicManager.Instance.music);
+		}
+
+		Time.timeScale = 1.0f;
+		SceneManager.LoadScene ("levelSelectEndless");
+		click.Play ();
+
 	}
 
 	//load from level select, level complete and credits
