f5c80fd84b29b0f7c4f28c2ad8847d5a077479e2
Catherine Claudet <cmclaudet@gmail.com>
2017-03-17 16:44:29 +0100

    Arcade and endless buttons not enabled when their scene is already selected

---
+++ Assets/arcadeAndEndless.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class arcadeAndEndless : MonoBehaviour {
+	public Button arcade;
+	public Button endless;
+	// Use this for initialization
+	void Start () {
+		string activeScene = SceneManager.GetActiveScene ().name;
+
+		if (activeScene == "levelSelect") {
+			arcade.enabled = false;
+			endless.enabled = true;
+		} else {
+			arcade.enabled = true;
+			endless.enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
