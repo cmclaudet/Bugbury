89195a53e007eed3256e80eb35b2b5c95cccbacc
Catherine Claudet <cmclaudet@gmail.com>
2017-03-22 14:34:39 +0100

    New perfect images and rotation
    - Added two new perfect images, one for background and one for text
    - Added rotation to perfect background image

---
+++ Assets/rotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+	private float rotationSpeed = 10.0f;
+	private Vector3 rotationEuler;
+
+	// Update is called once per frame
+	void Update () {
+		rotationEuler+= Vector3.forward*rotationSpeed*Time.deltaTime; //increment 30 degrees every second
+		transform.localRotation = Quaternion.Euler(rotationEuler);
+	}
+}
