916ff6700e9e5633285b8bb6d5261339687c75c7
Catherine Claudet <cmclaudet@gmail.com>
2017-03-22 14:41:34 +0100

    Added new perfect UI to prefabs
    - Prefab level complete and level complete level 5 have new perfect UI
    - Increased rotation speed of perfect background

--- Assets/rotate.cs
+++ Assets/rotate.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class rotate : MonoBehaviour {
 
-	private float rotationSpeed = 10.0f;
+	private float rotationSpeed = 15.0f;
 	private Vector3 rotationEuler;
 
 	// Update is called once per frame
