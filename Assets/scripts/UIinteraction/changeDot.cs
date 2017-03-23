ad06b89e3d2cd8bec7162da5fbffced5641de079
Catherine Claudet <cmclaudet@gmail.com>
2017-03-23 11:26:28 +0100

    Highlighted dot shows which level is being selected
    - Highlighted dot moves as player moves between hovering over different
    level select menus

---
+++ Assets/scripts/UIinteraction/changeDot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeDot : MonoBehaviour {
+
+	public Sprite whiteDot;
+	public Sprite darkDot;
+	private Image[] allDots;
+	// Use this for initialization
+	void Start () {
+		allDots = GetComponentsInChildren<Image>();
+		allDots[0].sprite = whiteDot;
+	}
+	
+	// Update is called once per frame
+	public void switchDotImage(int nextLevelNumber, int lastLevelNumber) {
+		allDots[nextLevelNumber - 1].sprite = whiteDot;
+		allDots[lastLevelNumber - 1].sprite = darkDot;
+	}
+}
