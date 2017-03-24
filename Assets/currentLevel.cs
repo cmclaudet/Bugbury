4393bf2270aa1465aad6a918f7647a8f5fd00ce4
Catherine Claudet <cmclaudet@gmail.com>
2017-03-24 14:43:49 +0100

    Added game object with current level public variable attached

---
+++ Assets/currentLevel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class currentLevel : MonoBehaviour {
+	public int levelNow{get;set;}
+
+	void Start() {
+		levelNow = 1;
+	}
+}
