b3362d9bfa27e9e9dd5f5808e6b6b3635c1c128d
Catherine Claudet <cmclaudet@gmail.com>
2017-04-03 15:31:15 +0200

    Keep buttons non interactable until final object is done scaling

---
+++ Assets/activateButtons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//[SerializeField]
+public class activateButtons : MonoBehaviour {
+
+	public Button[] buttons;
+	
+	// Update is called once per frame
+	void Update () {
+		if (GetComponent<scaleSetup>().doneScaling) {
+			foreach (Button button in buttons) {
+				button.interactable = true;
+			}
+		}
+	}
+}
